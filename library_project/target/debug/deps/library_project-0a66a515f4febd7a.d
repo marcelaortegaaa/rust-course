1b1d591bbc07941b15e63395a77d1de54bbefdaf
Marcela Ortega <marcelaortega174@gmail.com>
2025-10-08 12:56:53 -0500

    Added targets to Makefile

---
+++ library_project/target/debug/deps/library_project-0a66a515f4febd7a.d
@@ -0,0 +1,7 @@
+/Users/marcelaortegaaa/Documents/rust/library_project/target/debug/deps/library_project-0a66a515f4febd7a.d: src/lib.rs
+
+/Users/marcelaortegaaa/Documents/rust/library_project/target/debug/deps/liblibrary_project-0a66a515f4febd7a.rlib: src/lib.rs
+
+/Users/marcelaortegaaa/Documents/rust/library_project/target/debug/deps/liblibrary_project-0a66a515f4febd7a.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ library_project/target/debug/deps/library_project-cc7ca468ec38aec2.d
@@ -0,0 +1,5 @@
+/Users/marcelaortegaaa/Documents/rust/library_project/target/debug/deps/library_project-cc7ca468ec38aec2.d: src/lib.rs
+
+/Users/marcelaortegaaa/Documents/rust/library_project/target/debug/deps/library_project-cc7ca468ec38aec2: src/lib.rs
+
+src/lib.rs:
