97a2227dd6aa01cfafaad25814e9e0e9b62c2be4
Marcela Ortega <marcelaortega174@gmail.com>
2025-10-13 16:25:52 -0500

    Added file exploring vectors from the Data Engineering with Rust course

---
+++ src/bin/dataeng_vectors.rs
@@ -0,0 +1,28 @@
+use rand::rng;
+use rand::seq::SliceRandom;
+
+fn main() {
+    let mut fruit = vec![
+        "Orange",
+        "Fig",
+        "Pomegranate",
+        "Cherry",
+        "Apple",
+        "Pear",
+        "Peach",
+    ];
+
+    // Scramble (shuffle) the fruit
+    let mut rng = rng();
+    fruit.shuffle(&mut rng);
+
+    // Print out the fruit salad
+    println!("Fruit Salad:");
+    for (i, item) in fruit.iter().enumerate() {
+        if i != fruit.len() - 1 {
+            print!("{}, ", item);
+        } else {
+            println!("{}", item);
+        }
+    }
+}
