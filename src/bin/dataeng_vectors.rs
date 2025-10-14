3e306b48953408c691e1d556ac9766a148b318e9
Marcela Ortega <marcelaortega174@gmail.com>
2025-10-14 18:00:17 -0500

    Added function to get number of fruits in the salad and deliver

--- src/bin/dataeng_vectors.rs
+++ src/bin/dataeng_vectors.rs
@@ -1,11 +1,27 @@
 // To Do:
 // [X] Let user add fruits to the salad
 // [X] Select a random fruit with .choose from rand
-// [ ] Select a number of random fruits from a list and add to salad
+// [X] Select a number of random fruits from a list and add to salad
 
 use rand::rng;
 use rand::seq::{IndexedRandom, SliceRandom};
-use std::io;
+use std::io::{self, Write};
+
+fn read_salad_size() -> usize {
+    loop {
+        io::stdout().flush().ok();
+        println!("How many fruits would you like in your fruit salad?");
+        let mut s = String::new();
+        if io::stdin().read_line(&mut s).is_err() {
+            eprintln!("Failed to read input.");
+            continue;
+        }
+        match s.trim().parse::<usize>() {
+            Ok(n) => return n,
+            Err(_) => eprintln!("Please type a non-negative whole number."),
+        }
+    }
+}
 
 fn main() {
     let mut fruit = vec![
@@ -18,10 +34,9 @@ fn main() {
         "Peach".to_string(),
     ];
 
-    let mut extra_fruit = String::new();
-
     println!("Welcome to the Fruit Salad Bar");
     loop {
+        let mut extra_fruit = String::new();
         extra_fruit.clear();
         println!("\nHere are the fruits we currently have");
         for (i, fr) in fruit.iter().enumerate() {
@@ -49,16 +64,20 @@ fn main() {
     let mut rng = rng();
     fruit.shuffle(&mut rng);
 
-    // Pick a random fruit
-    println!("Here's a random fruit: {}", fruit.choose(&mut rng).unwrap());
+    let fruit_number = read_salad_size();
 
-    // // Print out the fruit salad
-    // println!("Fruit Salad:");
-    // for (i, item) in fruit.iter().enumerate() {
-    //     if i != fruit.len() - 1 {
-    //         print!("{}, ", item);
-    //     } else {
-    //         println!("{}", item);
-    //     }
-    // }
+    let fruit_salad: Vec<String> = fruit
+        .choose_multiple(&mut rng, fruit_number)
+        .cloned()
+        .collect();
+
+    // Print out the fruit salad
+    println!("Here's your fruit salad:");
+    for (i, item) in fruit_salad.iter().enumerate() {
+        if i != fruit_salad.len() - 1 {
+            print!("{}, ", item);
+        } else {
+            println!("{}", item);
+        }
+    }
 }
