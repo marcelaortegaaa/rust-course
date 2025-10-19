78018e01b0cacb8289c8a4598d0520fb0ad97f50
Marcela Ortega <marcelaortega174@gmail.com>
2025-10-19 12:43:35 -0500

    allow user input

--- src/bin/dataeng_hashmaps.rs
+++ src/bin/dataeng_hashmaps.rs
@@ -1,4 +1,27 @@
+// [X] Let user pass input
+// [ ] Count words in a sentence
+// [ ] Sort result by frequency
+
+use clap::Parser;
 use std::collections::HashMap;
+
+#[derive(Parser)]
+#[command(
+    version = "1.0",
+    author = "Marcela ◡̈",
+    about = "Counter for numbers in a vector"
+)]
+struct Opts {
+    #[arg(
+        long,
+        value_delimiter = ',',
+        num_args = 1..,
+        value_parser = clap::value_parser!(i32),
+        default_values_t = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 3],
+        help = "Vector to count the numbers of"
+    )]
+    vector: Vec<i32>,
+}
 
 fn logic(numbers: Vec<i32>) -> Vec<(i32, u32)> {
     let mut frequencies = HashMap::new();
@@ -18,8 +41,11 @@ fn logic(numbers: Vec<i32>) -> Vec<(i32, u32)> {
 }
 
 fn main() {
-    let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 3];
-    let result = logic(numbers);
+    let opts: Opts = Opts::parse();
+    let user_input = opts.vector;
+
+    let result = logic(user_input);
+
     //print the results in a human readable format that explains what the result is.
     println!(
         "The frequency of each number in the vector is: {:?}",
