8b67e02a81cf27567f9a9cf571e32fee30d48264
Marcela Ortega <marcelaortega174@gmail.com>
2025-10-23 14:12:41 -0500

    priority queue code from rust docs

--- src/bin/dataeng_binaryheap.rs
+++ src/bin/dataeng_binaryheap.rs
@@ -1,1 +1,29 @@
-fn main() {}
+// From Rust docs
+use std::collections::BinaryHeap;
+
+fn main() {
+    let mut heap = BinaryHeap::new();
+    let push_vec = vec![3, 4, 5, 6, 2, 4, 5, 3];
+
+    heap.push(1);
+    heap.push(5);
+    heap.push(2);
+
+    heap.extend(push_vec);
+
+    // Peek shows the most important item in the heap
+    let peek = heap.peek();
+    println!("{peek:?}");
+
+    // Items are returned in a random order
+    // for x in &heap {
+    //     println!("{x}");
+    // }
+
+    // while let Some(item) = heap.pop() {
+    //     println!("{item}");
+    // }
+
+    let sorted_vec = heap.into_sorted_vec();
+    println!("{sorted_vec:?}");
+}
