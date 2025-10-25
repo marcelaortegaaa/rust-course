44dd0a92e23bebba0b9fdb96be2a22a0d897ef9a
Marcela Ortega <marcelaortega174@gmail.com>
2025-10-25 14:50:59 -0500

    moved code around to experiment with different collection types

--- src/bin/dataeng_hash_fruits.rs
+++ src/bin/dataeng_hash_fruits.rs
@@ -2,6 +2,7 @@ use rand::{rng, seq::IndexedRandom};
 use std::collections::HashSet;
 
 fn generate_fruit(n: usize) -> HashSet<&'static str> {
+    let mut rng = rng();
     let fruits = [
         "Apple",
         "Banana",
@@ -13,16 +14,23 @@ fn generate_fruit(n: usize) -> HashSet<&'static str> {
         "Honeydew",
     ];
 
-    let mut rng = rng();
+    let mut fruit_set = HashSet::new();
+    for _ in 0..n {
+        let fruit = fruits.choose(&mut rng).cloned().unwrap();
+        fruit_set.insert(fruit);
+    }
 
-    let fruit_set = fruits.choose_multiple(&mut rng, n).cloned().collect();
+    // This makes the HashSet unnecesary because the method doesn't have repetition
+    // let fruit_set = fruits.choose_multiple(&mut rng, n).cloned().collect();
+
     fruit_set
 }
 
 fn main() {
-    let n = 100;
+    let n = 30;
     let fruit_set = generate_fruit(n);
 
     println!("Generating {} random fruits...", n);
     println!("Number of unique fruits generated: {}", fruit_set.len());
+    println!("Fruit set: {:?}", fruit_set);
 }
