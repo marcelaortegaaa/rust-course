877b1b52959b2fd32b455f08b700992d030caba9
Marcela Ortega <marcelaortega174@gmail.com>
2025-11-18 18:02:13 -0500

    changed philosopher creation to simplify

--- src/bin/dataeng_diningphilosopher.rs
+++ src/bin/dataeng_diningphilosopher.rs
@@ -23,6 +23,8 @@
 * - Prints timing metrics for simulation
 */
 
+// Why do they eat with two forks? What?
+
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
@@ -33,28 +35,32 @@ struct Fork {
 }
 
 struct Philosopher {
-    id: u32,
     name: String,
     left_fork: Arc<Fork>,
     right_fork: Arc<Fork>,
 }
 
 impl Philosopher {
-    fn new(id: u32, name: &str, left_fork: Arc<Fork>, right_fork: Arc<Fork>) -> Philosopher {
-        Philosopher {
-            id,
-            name: name.to_string(),
-            left_fork,
-            right_fork,
+    fn new(name: &str, fork_a: Arc<Fork>, fork_b: Arc<Fork>) -> Philosopher {
+        if fork_a.id <= fork_b.id {
+            Philosopher {
+                name: name.to_string(),
+                left_fork: fork_a,
+                right_fork: fork_b,
+            }
+        } else {
+            Philosopher {
+                name: name.to_string(),
+                left_fork: fork_b,
+                right_fork: fork_a,
+            }
         }
+
+        // No ID needed if you make them take the lower fork first, no deadlock
     }
 
     fn eat(&self) {
-        let (first_fork, second_fork) = if self.id % 2 == 0 {
-            (&self.left_fork, &self.right_fork)
-        } else {
-            (&self.right_fork, &self.left_fork)
-        };
+        let (first_fork, second_fork) = (&self.left_fork, &self.right_fork);
 
         let _first_guard = first_fork.mutex.lock().unwrap();
         println!("{} picked up fork {}.", self.name, first_fork.id);
@@ -62,7 +68,7 @@ impl Philosopher {
         println!("{} picked up fork {}.", self.name, second_fork.id);
 
         println!("{} is eating.", self.name);
-        thread::sleep(Duration::from_secs(1));
+        thread::sleep(Duration::from_millis(500));
         println!("{} finished eating.", self.name);
 
         println!("{} put down fork {}.", self.name, first_fork.id);
@@ -101,14 +107,8 @@ fn main() {
         ("Thales of Miletus", 2, 3),
     ]
     .into_iter()
-    .enumerate()
-    .map(|(id, (name, left, right))| {
-        Philosopher::new(
-            id as u32,
-            name,
-            Arc::clone(&forks[left]),
-            Arc::clone(&forks[right]),
-        )
+    .map(|(name, left, right)| {
+        Philosopher::new(name, Arc::clone(&forks[left]), Arc::clone(&forks[right]))
     })
     .collect::<Vec<_>>();
 
